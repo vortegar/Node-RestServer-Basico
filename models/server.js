972ab877d661735de876513bb8a941b81af876c1
Victorio Ortega <vortegar28@gmail.com>
2023-04-29 18:38:00 -0400

    se agregan nuevas rutas y se implementa cloudinary

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const { dbContection } = require('../database/config');
 
 class Server {
@@ -14,7 +16,8 @@ class Server {
         buscar     : '/api/buscar',
         categorias : '/api/categorias',
         productos  : '/api/productos',
-        usuarios   : '/api/usuarios'
+        usuarios   : '/api/usuarios',
+        uploads    : '/api/uploads'
     }
     // Conectar a base de datos
     this.conectarDB();
@@ -39,6 +42,13 @@ class Server {
 
         //Middleware
         this.app.use( express.static('public'));
+
+        //Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes() {
@@ -47,6 +57,7 @@ class Server {
         this.app.use( this.paths.categorias, require('../routes/categorias'));
         this.app.use( this.paths.productos, require('../routes/productos'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use( this.paths.uploads, require('../routes/uploads'));
     }
 
     listen() {
